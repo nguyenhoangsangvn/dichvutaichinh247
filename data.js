cb8a41461522ff57717a50f3776548e912cee4d5
Shang <87207809+nguyenhoangsangvn@users.noreply.github.com>
2025-12-02 20:44:04 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,45 @@
+// data.js - Kho dữ liệu App
+const appData = [
+    {
+        id: "cayvang",
+        name: "Cây Vàng",
+        logo: "https://dichvutaichinh247.com/wp-content/uploads/2024/10/cay-vang-vn-768x768.png",
+        desc: "Vay nhanh lãi 0% - Top 1 hiện nay",
+        limit: "10 Triệu",
+        rate: "0% / 7 ngày",
+        // Thay Link Affiliate của ông vào đây
+        affLink: "https://dinos.scaletrk.com/click?a=2801&o=935",
+        // Link bài review chi tiết (để sau làm tiếp)
+        reviewLink: "#",
+        badge: "DUYỆT 99%",
+        badgeColor: "bg-green-100 text-green-700",
+        isHot: true
+    },
+    {
+        id: "jeff",
+        name: "Jeff App",
+        logo: "https://dichvutaichinh247.com/wp-content/uploads/2021/09/vay-tien-online-jeff-e1729183715589.png",
+        desc: "Robot tìm khoản vay tự động",
+        limit: "15 Triệu",
+        rate: "Thấp nhất",
+        affLink: "https://ladipage.dinos.click/click?a=2801&o=161",
+        reviewLink: "#",
+        badge: "AUTO DUYỆT",
+        badgeColor: "bg-blue-100 text-blue-700",
+        isHot: true
+    },
+    {
+        id: "moneycat",
+        name: "MoneyCat",
+        logo: "https://dichvutaichinh247.com/wp-content/uploads/2022/08/moneycat-logo-667x400.jpeg",
+        desc: "0% Lãi suất cho khách mới",
+        limit: "5 Triệu",
+        rate: "0% Lãi",
+        affLink: "https://dinos.scaletrk.com/click?a=2801&o=762",
+        reviewLink: "#",
+        badge: "UY TÍN",
+        badgeColor: "bg-purple-100 text-purple-700",
+        isHot: false
+    },
+    // Muốn thêm app thì copy đoạn trên dán xuống đây
+];
